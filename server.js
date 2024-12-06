8a1ff32546316c8dd26468c0f986eb6c6a46b0be
sai-sreeram-nanapu <nanapusaisreeram2002@gmail.com>
2024-12-06 00:43:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require('./auth');
+const authRoutes = require('./routes/auth');
 
 dotenv.config();
 const app = express();
